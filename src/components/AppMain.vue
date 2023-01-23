83ddce745608e02441ef2631c487101817fd3150
AndreaChiari <chiariandrea94@gmail.com>
2023-01-23 14:32:12 +0100

    fine-esercizio

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,13 @@
 <script>
+
+import MainCard from './MainCard.vue'
+
 export default {
-    name: 'Main'
+    name: 'Main',
+    components: { MainCard },
+    props: {
+        cards: Array
+    }
 }
 </script>
 
@@ -8,7 +15,8 @@ export default {
     <main>
         <div>
             <section class="content">
-                <h1> -->Content goes here </h1>
+                <MainCard v-for="card in cards" :key="card.series" :card="card"></MainCard>
+                <button> LOAD MORE </button>
             </section>
         </div>
         <section></section>
@@ -19,9 +27,23 @@ export default {
 @use '../assets/scss/colors.scss' as *;
 @use '../assets/scss/mixin.scss' as *;
 
+
+button {
+    border: 1px solid $color-blue;
+    padding: 5px 40px;
+    background-color: $color-blue;
+    color: $color-white;
+    cursor: pointer;
+    position: absolute;
+    bottom: 2%;
+    left: 50%;
+    transform: translate(-50%);
+}
+
 main {
     position: relative;
     z-index: 2;
+
 }
 
 div {
@@ -30,15 +52,19 @@ div {
 
     .content {
         @include container;
-        height: 100px;
+        min-height: 100px;
+        display: flex;
+        flex-wrap: wrap;
+        padding-top: 20px;
+        padding-bottom: 60px;
 
 
 
         h1 {
-
             padding-top: 30px;
             color: $color-white;
         }
+
     }
 
 }
---
+++ src/components/MainCard.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'MainCard',
+    props: {
+        card: Object
+    }
+};
+</script>
+<template>
+    <div class="card">
+        <img :src="card.thumb" alt="card.type">
+        <h3>{{ card.series }}</h3>
+    </div>
+</template>
+<style scoped lang="scss">
+.card {
+    flex-basis: calc(100% / 6);
+}
+
+img {
+    display: block;
+
+}
+
+h3 {
+    color: white;
+    padding-right: 2px;
+}
+</style>
